7378b032b5899cd2e133b4fb62777b44f93c4f51
myalcinkuru <yalcin.kuru@smartface.io>
2021-03-10 14:35:49 +0300

    modify biometic login imp

--- app/src/main/java/smartface/com/nativetest/BiometricLoginTest.java
+++ app/src/main/java/smartface/com/nativetest/BiometricLoginTest.java
@@ -1,18 +1,23 @@
 package smartface.com.nativetest;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 
+
+import com.google.android.exoplayer2.util.Log;
 
 import java.util.concurrent.Executor;
 
@@ -23,22 +28,25 @@ public class BiometricLoginTest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_native_test);
 
-
+        Context ctx = this;
         Executor executor = ContextCompat.getMainExecutor(this);
         BiometricPrompt biometricPrompt = new BiometricPrompt(this, executor, new BiometricPrompt.AuthenticationCallback() {
                     @Override
                     public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                        Toast.makeText(ctx, "SUCCESS " , Toast.LENGTH_SHORT).show();
                         super.onAuthenticationSucceeded(result);
                     }
 
                     @Override
                     public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
                         super.onAuthenticationError(errorCode, errString);
+                        Toast.makeText(ctx, "Error " + errString, Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
                     public void onAuthenticationFailed() {
                         super.onAuthenticationFailed();
+                        Toast.makeText(ctx, "FAILED !! " , Toast.LENGTH_SHORT).show();
                     }
                 });
 
@@ -52,26 +60,22 @@ public class BiometricLoginTest extends AppCompatActivity {
 
         btn.setOnClickListener((view) -> {
 
-//            BiometricManager biometricManager = BiometricManager.from(this);
-//            int authenticateType = biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG);
-//            switch (authenticateType) {
-//                case BiometricManager.BIOMETRIC_SUCCESS:
-//                    Log.d("MY_APP_TAG", "App can authenticate using biometrics.");
-//                    break;
-//                case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
-//                    Log.e("MY_APP_TAG", "No biometric features available on this device.");
-//                    break;
-//                case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
-//                    Log.e("MY_APP_TAG", "Biometric features are currently unavailable.");
-//                    break;
-//                case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
-//                    // Prompts the user to create credentials that your app accepts.
-////                    final Intent enrollIntent = new Intent(Settings.ACTION_BIOMETRIC_ENROLL);
-////                    enrollIntent.putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
-////                            BIOMETRIC_STRONG | DEVICE_CREDENTIAL);
-////                    startActivityForResult(enrollIntent, REQUEST_CODE);
-//                    break;
-//            }
+            BiometricManager biometricManager = BiometricManager.from(this);
+            int authenticateType = biometricManager.canAuthenticate();
+            switch (authenticateType) {
+                case BiometricManager.BIOMETRIC_SUCCESS:
+                    Toast.makeText(this, "App can authenticate using biometrics.", Toast.LENGTH_SHORT).show();
+                    break;
+                case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                    Toast.makeText(this, "No biometric features available on this device.", Toast.LENGTH_SHORT).show();
+                    break;
+                case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                    Toast.makeText(this, "Biometric features are currently unavailable.", Toast.LENGTH_SHORT).show();
+                    break;
+                case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                    Toast.makeText(this, "BIOMETRIC_ERROR_NONE_ENROLLED", Toast.LENGTH_SHORT).show();
+                    break;
+            }
 
             biometricPrompt.authenticate(promptInfo);
         });
